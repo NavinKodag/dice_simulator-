87010f42a86b5e2f49b4765316305ef42295eee1
Navin Kodag <48642117+NavinKoodag@users.noreply.github.com>
2019-07-20 16:57:48 +0530

    Add files via upload

---
+++ dice_simulator.py
@@ -0,0 +1,66 @@
+import random
+import time
+
+def dice_roll():
+    counter=0
+    list=[
+                                           '''
+                                                -----
+                                                |   |
+                                                | o |    = 1
+                                                |   |
+                                                -----''',
+
+                                           '''
+                                                -----
+                                                |o  |
+                                                |   |     = 2
+                                                |  o|
+                                                -----''',
+
+                                           '''
+                                                -----
+                                                |o  |
+                                                | o |     = 3
+                                                |  o|
+                                                -----''',
+
+                                           '''
+                                                -----
+                                                |o o|
+                                                |   |     = 4
+                                                |o o|
+                                                -----''',
+
+                                           '''
+                                                -----
+                                                |o o|
+                                                | o |     = 5
+                                                |o o|
+                                                -----''',
+
+                                           '''
+                                                -----
+                                                |o o|
+                                                |o o|     = 6
+                                                |o o|
+                                                ----- ''',
+                                                 ]
+    while  counter !=6:
+           counter =counter + 1
+           time.sleep(0.49)
+           print(chr(27) + "[2J")
+           print(random.choice(list))
+
+dice_roll()
+
+num = 3
+print("input  'quit' to stop  ")
+print("Press any key to roll the dice ")
+while num!='quit':
+    num=input(">>> ")
+    if num == 'quit':
+        break
+    else:
+        dice_roll()
+        print(":)")
